0b2325d5283063249bd098348e094ab40fae6a93
Chad Pry <chad.pry@gmail.com>
2011-12-23 23:12:24 -0600

    + fix bug

--- lib/phone-insight/application.rb
+++ lib/phone-insight/application.rb
@@ -28,7 +28,7 @@ module PhoneInsight
 
     get "/:number/:name" do
       require 'cgi'
-      url = "http://phone-insight.heroku.com/api/twilio/personal?#{CGI.escape(params[:url] || "http://phone-insight.heroku.com/clips/aqua-teen-hunger-force.mp3")}"
+      url = "http://phone-insight.heroku.com/api/twilio/personal?url=#{CGI.escape(params[:url] || "http://phone-insight.heroku.com/clips/aqua-teen-hunger-force.mp3")}"
       TWILIO_CLIENT.account.calls.create({
         :from => TWILIO["from"], :to => "+#{params[:number]}",
         :url => url
@@ -39,7 +39,7 @@ module PhoneInsight
     post "/api/twilio/personal" do
       content_type :xml
       erb :"twilio/play_a_clip", :locals => {
-        :clip => params[:sound_url]
+        :clip => params[:url]
       }
 #      Helpers::TwilioHelper.personal
     end
